e8b708939224c72b5b14c27952a089e00dc262b8
Tatyana Chernyadieva <tany.taray@gmail.com>
2023-02-17 15:14:47 +0400

    Spring IoC (DI) annotation
    добавлена ветка spring-value-annotation по использованию аннотаций @Value,@@PostConstruct/@PreDestroy,@Scope

--- src/main/java/ru/home/chernyadieva/audio/AudioSystem.java
+++ src/main/java/ru/home/chernyadieva/audio/AudioSystem.java
@@ -1,24 +1,49 @@
 package ru.home.chernyadieva.audio;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import ru.home.chernyadieva.music.player.MusicPlayer;
 
-import java.util.UUID;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Component
+@Scope("singleton")
 public class AudioSystem {
-    private String id;
     private MusicPlayer musicPlayer;
+
+    @Value("${audioSystem.id}")
+    private String id;
+
+    @Value("${audioSystem.volume}")
+    private int volume;
 
     @Autowired
     public AudioSystem(MusicPlayer musicPlayer) {
-        this.id = UUID.randomUUID().toString();
         this.musicPlayer = musicPlayer;
+    }
+
+    @PostConstruct
+    private void doInit(){
+        System.out.println("Initialization beans");
+    }
+    @PreDestroy
+    private void doDestroy(){
+        System.out.println("Destroy beans");
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getVolume() {
+        return volume;
     }
 
     @Override
     public String toString() {
-        return musicPlayer.playMusic();
+        return musicPlayer.playMusic() + "\nvolume: " + volume + "\nid: " + id;
     }
 }
--- src/main/java/ru/home/chernyadieva/spring/TestSpring.java
+++ src/main/java/ru/home/chernyadieva/spring/TestSpring.java
@@ -9,7 +9,10 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         AudioSystem audioSystem = context.getBean("audioSystem", AudioSystem.class);//получаем бин из спринг контекста
+        AudioSystem audioSystem2 = context.getBean("audioSystem", AudioSystem.class);//получаем бин из спринг контекста
         System.out.println(audioSystem);
+        System.out.println(audioSystem2);
+        System.out.println("Objects are equal? -> " + (audioSystem == audioSystem2));
 
         context.close();//закрываем контекст
     }
